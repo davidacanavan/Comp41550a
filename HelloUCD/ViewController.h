3d27ba7a44e36599b4213293c7c1274e911be23f
David Canavan <d.i.dave.canavan@gmail.com>
2013-01-16 13:07:00 +0000

    Initial Commit

---
+++ HelloUCD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloUCD
+//
+//  Created by David Canavan on 16/01/2013.
+//  Copyright (c) 2013 David Canavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloUCD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloUCD
+//
+//  Created by David Canavan on 16/01/2013.
+//  Copyright (c) 2013 David Canavan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
