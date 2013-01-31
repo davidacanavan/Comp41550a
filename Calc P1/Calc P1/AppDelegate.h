3e5213a32fc56b26239b0f52ce730cab245395a2
David Canavan <d.i.dave.canavan@gmail.com>
2013-01-31 19:52:48 +0000

    Done so far as from the walk-through

---
+++ Calc P1/Calc P1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calc P1
+//
+//  Created by David Canavan on 31/01/2013.
+//  Copyright (c) 2013 ie.ucd.csi.comp41550. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
