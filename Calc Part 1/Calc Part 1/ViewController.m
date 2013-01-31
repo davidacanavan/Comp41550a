3996f0da237b867b1e4d3a65d5fa6d7724f5aa15
David Canavan <d.i.dave.canavan@gmail.com>
2013-01-31 19:28:55 +0000

    Part 1 complete

--- Calc Part 1/Calc Part 1/ViewController.m
+++ Calc Part 1/Calc Part 1/ViewController.m
@@ -8,11 +8,37 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+
+@synthesize calcModel = _calcModel;
+@synthesize calcDisplay = _calcDisplay;
+@synthesize isInTheMiddleOfTypingSomething = _isInTheMiddleOfTypingSomething;
+
+- (IBAction)digitPressed:(UIButton *)sender
+{
+    NSString *digit = sender.titleLabel.text;
+    
+    if(self.isInTheMiddleOfTypingSomething)
+        self.calcDisplay.text = [self.calcDisplay.text stringByAppendingString:digit];
+    else
+    {
+        [self.calcDisplay setText:digit];
+        self.isInTheMiddleOfTypingSomething = YES;
+    }
+}
+
+- (IBAction)operationPressed:(UIButton *)sender
+{
+    if(self.isInTheMiddleOfTypingSomething)
+    {
+        self.calcModel.operand = [self.calcDisplay.text doubleValue];
+        self.isInTheMiddleOfTypingSomething = NO;
+    }
+    
+    NSString *operation = sender.titleLabel.text;
+    double result = [self.calcModel performOperation:operation];
+    self.calcDisplay.text = [NSString stringWithFormat:@"%g", result];
+}
 
 - (void)viewDidLoad
 {
