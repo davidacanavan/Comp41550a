f36f3c43c0a53775cad7c12bd3b627f4baab3551
David Canavan <d.i.dave.canavan@gmail.com>
2013-04-22 16:04:28 +0100

    panning done

--- Calc P4/Calc P4/GraphView.m
+++ Calc P4/Calc P4/GraphView.m
@@ -12,26 +12,33 @@
     if (!_isAxesOriginSet)
         _axesOrigin = CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect));
     
+    // Get some stats about the bounds
+    CGFloat width = rect.size.width;
+    CGFloat height = rect.size.height;
+    float xCentre = width / 2, yCentre = height / 2;
+    CGPoint axesShift = CGPointMake(xCentre - _axesOrigin.x, _axesOrigin.y - yCentre); // Cartesian style
+    
     [AxesDrawer drawAxesInRect:rect originAtPoint:_axesOrigin scale:_scale];
     
     // Draw the function!
 	CGContextRef context = UIGraphicsGetCurrentContext();
 	UIGraphicsPushContext(context);
     
-    CGFloat width = rect.size.width;
-    CGFloat height = rect.size.height;
-    float xMax = width / 2 / _scale, xMin = -xMax; // Provided it's all centered
-    float xCentre = width / 2, yCentre = height / 2;
+    float xMax = (axesShift.x + width / 2) / _scale;
+    float xMin = (axesShift.x -width / 2) / _scale; // Provided it's all centered
     float callStep = (xMax - xMin) / 1000; // Lets give this a high enough resolution
     
-    CGPoint previous = CGPointMake(xCentre + xMin * _scale, height - ([_dataSource evaluateFunctionAt:xMin] * _scale + yCentre));
+    CGPoint previous = CGPointMake(xCentre - axesShift.x + xMin * _scale, height - ([_dataSource evaluateFunctionAt:xMin] * _scale + yCentre - axesShift.y));
+    //NSLog(@"Start!");
+    NSLog(@"%f", axesShift.x);
+    NSLog(@"%@", NSStringFromCGPoint(CGPointMake(xMin, [_dataSource evaluateFunctionAt:xMin])));
     CGContextBeginPath(context);
     CGContextMoveToPoint(context, previous.x, previous.y);
     
     for (float x = xMin + callStep; x <= xMax + 1; x += callStep)
     {
-        CGPoint current = CGPointMake(xCentre + x * _scale,
-            height - ([_dataSource evaluateFunctionAt:x] * _scale + yCentre));
+        CGPoint current = CGPointMake(xCentre - axesShift.x + x * _scale,
+                                      height - ([_dataSource evaluateFunctionAt:x] * _scale + yCentre - axesShift.y));
         CGContextAddLineToPoint(context, current.x, current.y);
         previous = current;
     }
